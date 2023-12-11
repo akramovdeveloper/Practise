f977e0140003f7de642c997ddbf415be9233c009
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-12-12 01:49:54 +0500

    solved some tasks

---
+++ Practise/LeetCode/Medium/_151ReverseWordsinaString/Solution151_2.cs
@@ -0,0 +1,25 @@
+﻿namespace Practise.LeetCode.Medium._151ReverseWordsinaString;
+public class Solution151_2
+{
+    public static string ReverseWords(string s)
+    {
+        var begin = 0;
+        var end = 0;
+        List<string> words = new List<string>();
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+            {
+                if (begin != end)
+                    words.Add(s.Substring(begin, end-begin));
+                end++;
+                begin = end;
+            }
+            else end++;
+        }
+        if (begin != end)
+            words.Add(s.Substring(begin, end-begin));
+        words.Reverse();
+        return string.Join(" ", words.ToArray());
+    }
+}
--- Practise/Program.cs
+++ Practise/Program.cs
@@ -1,5 +1,7 @@
-﻿using Practise.LeetCode.Easy._345ReverseVowelsofaString;
+﻿using Practise._15._3Sum;
+using Practise.LeetCode.Easy._345ReverseVowelsofaString;
 using Practise.LeetCode.Easy._605CanPlaceFlowers;
+using Practise.LeetCode.Medium._151ReverseWordsinaString;
 
 namespace Practise;
 
@@ -7,7 +9,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Solution345.ReverseVowels("leetcode"));
+        Console.WriteLine(Solution151_2.ReverseWords("the sky is blue"));
 
     }
 }
