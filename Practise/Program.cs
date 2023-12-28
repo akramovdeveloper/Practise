bf6613ad6698d47f49b575a7522a4f40754d619d
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-12-28 22:05:55 +0500

    fixed

--- Practise/Program.cs
+++ Practise/Program.cs
@@ -2,6 +2,7 @@
 using Practise.LeetCode.Medium._443StringCompression;
 using Practise.BankInfoFromOracle;
 using Practise.LeetCode.Medium._238ProductofArrayExceptSelf;
+using Practise.PostgresOracle;
 
 namespace Practise;
 
@@ -9,13 +10,13 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        char[] x = new char[] { 'a','a','a', 'b', 'b', 'a', 'a' };
-        Console.WriteLine(Solution443_2.Compress(x));
+        //char[] x = new char[] { 'a','a','a', 'b', 'b', 'a', 'a' };
+        //Console.WriteLine(Solution443_2.Compress(x));
         /*var array = Solution238.ProductExceptSelf(-1,1,0,-3,3);
         foreach (var item in array)
             Console.Write(item + " ");*/
 
-        BankInfo.BankInfoWithJoin();
+        ConnectTwoBase.ConnectTwoBaseMethod();
 
     }
 }
